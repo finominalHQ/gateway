8a3fadb73daf1a2c6111e4117c894b670ddba339
Isaiah Iroko <isaiahiroko@gmail.com>
2022-08-31 11:43:28 +0100

    update enums

--- pkg/route/index.go
+++ pkg/route/index.go
@@ -3,23 +3,35 @@ package route
 type AuthType string
 
 var (
-	NO_AUTH    = "NO_AUTH"
-	BASIC_AUTH = "BASIC_AUTH"
-	TOKEN_AUTH = "TOKEN_AUTH"
-	PIN_AUTH   = "PIN_AUTH"
-	OTP_AUTH   = "OTP_AUTH"
+	NO_AUTH    AuthType = "NO_AUTH"
+	BASIC_AUTH AuthType = "BASIC_AUTH"
+	TOKEN_AUTH AuthType = "TOKEN_AUTH"
+	PIN_AUTH   AuthType = "PIN_AUTH"
+	OTP_AUTH   AuthType = "OTP_AUTH"
 )
+
+func (s AuthType) String() string {
+	return string(s)
+}
 
 type StatusType string
 
 const (
-	ACTIVE   = "ACTIVE"
-	INACTIVE = "INACTIVE"
+	ACTIVE   StatusType = "ACTIVE"
+	INACTIVE StatusType = "INACTIVE"
 )
+
+func (s StatusType) String() string {
+	return string(s)
+}
 
 type TypeType string
 
 const (
-	INCOMING = "INCOMING"
-	OUTGOING = "OUTGOING"
+	INCOMING TypeType = "INCOMING"
+	OUTGOING TypeType = "OUTGOING"
 )
+
+func (s TypeType) String() string {
+	return string(s)
+}
